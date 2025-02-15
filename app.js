910cb08371f5d0255dc40a17ffd9ee3e6f4318bc
Michelle Castillo <mcdiaz3@uc.cl>
2025-02-15 20:19:42 -0300

    Versión corregida con 2 funciones listas y código innecesario eliminado

--- app.js
+++ app.js
@@ -7,37 +7,30 @@ function asignarTextoElemento (elemento, texto) {
     elementoHTML.innerHTML = texto;
     return;
 }
+function agregarAmigo() {
+  let amigoUsuario = document.getElementById("amigo").value;
 
-function agregarAmigo () {
-    let amigoUsuario = document.getElementById("amigo").value; //Capturar el valor del campo de entrada
-    if (amigoUsuario === "") { 
-        alert("Por favor, inserte un nombre"); //Validar la entrada: asegura que el campo no esté vacío. Muestra una alerta con un mensaje de error
-    } else {
-        nombreAmigos.push (amigoUsuario); // Actualiza el array de nombreAmigos: almacena los nombre de amigos usando el método.push().
-        document.getElementById("añadir").setAttribute("enabled","true"); 
-        return amigoUsuario;
-    }
-    limpiarCaja (); //Limpia el campo de entrada
+  if (amigoUsuario === "") {
+    alert("Por favor, ingresa el nombre de tu amigo secreto.");
+  } else {
+    nombreAmigos.push(amigoUsuario); // Agrega el nuevo amigo al arreglo
+    actualizaAmigos(); // Llama a la función para actualizar la lista
+    limpiarCaja(); 
+  }
+}
+
+function actualizaAmigos() {
+  let listaVista = document.getElementById("listaAmigos");
+  listaVista.innerHTML = ""; // Limpia el contenido actual de la lista
+
+  for (let i = 0; i < nombreAmigos.length; i++) {
+    const nuevoItem = document.createElement('li');
+    nuevoItem.textContent = nombreAmigos[i];
+    listaVista.appendChild(nuevoItem);
+  }
 }
 
 function limpiarCaja () {
     document.querySelector("#amigo").value = "";
 }
 
-/* Tareas específicas:
-
-Obtener el elemento de la lista: Utilizar document.getElementById() o document.querySelector() para seleccionar la lista donde se mostrarán los amigos.
-
-Limpiar la lista existente: Establecer lista.innerHTML = "" para asegurarse de que no haya duplicados al actualizar.
-
-Iterar sobre el arreglo: Usa un bucle for para recorrer el arreglo amigos y crear elementos de lista (<li>) para cada título.
-
-Agregar elementos a la lista: Para cada amigo, crear un nuevo elemento de lista.*/
-
-function actualizaAmigos () {
-    
-} 
-
-function sortearAmigo () {
-
-}
