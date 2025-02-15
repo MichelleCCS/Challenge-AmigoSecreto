a30541b5d73b500421b3caaa6912b46aff2d82c3
Michelle Castillo <mcdiaz3@uc.cl>
2025-02-15 19:50:37 -0300

    Avance en una de las funciones y en botón añadir

--- app.js
+++ app.js
@@ -9,12 +9,34 @@ function asignarTextoElemento (elemento, texto) {
 }
 
 function agregarAmigo () {
-
+    let amigoUsuario = document.getElementById("amigo").value; //Capturar el valor del campo de entrada
+    if (amigoUsuario === "") { 
+        alert("Por favor, inserte un nombre"); //Validar la entrada: asegura que el campo no esté vacío. Muestra una alerta con un mensaje de error
+    } else {
+        nombreAmigos.push (amigoUsuario); // Actualiza el array de nombreAmigos: almacena los nombre de amigos usando el método.push().
+        document.getElementById("añadir").setAttribute("enabled","true"); 
+        return amigoUsuario;
+    }
+    limpiarCaja (); //Limpia el campo de entrada
 }
+
+function limpiarCaja () {
+    document.querySelector("#amigo").value = "";
+}
+
+/* Tareas específicas:
+
+Obtener el elemento de la lista: Utilizar document.getElementById() o document.querySelector() para seleccionar la lista donde se mostrarán los amigos.
+
+Limpiar la lista existente: Establecer lista.innerHTML = "" para asegurarse de que no haya duplicados al actualizar.
+
+Iterar sobre el arreglo: Usa un bucle for para recorrer el arreglo amigos y crear elementos de lista (<li>) para cada título.
+
+Agregar elementos a la lista: Para cada amigo, crear un nuevo elemento de lista.*/
 
 function actualizaAmigos () {
-
-}
+    
+} 
 
 function sortearAmigo () {
 
